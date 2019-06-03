500ab0160907779067890e78fc4ff9073e546cb5
jiangseventeen <jiangseventeen@gmail.com>
2019-06-03 21:09:56 +0800

    初始化前端项目

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <the-top-heading/>
+    <the-left-sidebar/>
+    <the-right-sidebar/>
+    <the-footer/>
+  </div>
+</template>
+
+<script>
+import TheTopHeading from './layout/TheTopHeading';
+import TheLeftSidebar from './layout/TheLeftSidebar';
+import TheRightSidebar from './layout/TheRightSidebar';
+import TheFooter from './layout/TheFooter';
+
+export default {
+  name: 'app',
+  components: {
+    TheTopHeading,
+    TheLeftSidebar,
+    TheRightSidebar,
+    TheFooter
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+  overflow: hidden;
+}
+#app {
+  width: 100%;
+  height: 100%;
+  background: url(./assets/image/app-bg.png) repeat;
+}
+</style>
