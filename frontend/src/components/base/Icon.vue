7129359fff17dd4a07ea17b09a30a2c9e01717da
jiangseventeen <jiangseventeen@gmail.com>
2019-06-03 21:10:29 +0800

    添加图标和标题组件

---
+++ frontend/src/components/base/Icon.vue
@@ -0,0 +1,12 @@
+<template>
+  <i :class="['fas', type]"></i>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: ['type']
+}
+</script>
+
+<style scope src="@fortawesome/fontawesome-free/css/all.css">
---
+++ frontend/src/components/base/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="common-title">
+    <slot></slot>
+  </h1>
+</template>
+
+<style lang="scss" scope>
+@import 'src/assets/scss/_variables';
+
+.common-title {
+  font: {
+    size: 14px;
+    weight: 300;
+  }
+  margin: 0;
+  padding: 5px 10px;
+  text-align: center;
+  color: $primary-text-color;
+  background: #353a42;
+}
+</style>
