1965c1265a359415d7b25d8f312f940aa78829ca
jiangseventeen <jiangseventeen@gmail.com>
2019-06-11 22:22:12 +0800

    完成底部缩放工具栏

--- frontend/src/layout/TheFooter.vue
+++ frontend/src/layout/TheFooter.vue
@@ -1,9 +1,42 @@
 <template>
   <div class="footer">
-
+    <div class="footer-resize">
+      <span class="icon-button-minus" @click="zoomDown"><i class="fa fa-search-minus"></i></span>
+      <div class="footer-slider">
+        <el-slider v-model="pageScale" :show-tooltip="true"></el-slider>
+      </div>
+      <span class="icon-button-add" @click="zoomUp"><i class="fa fa-search-plus"></i></span>
+      <span class="footer-size-text" v-text="pageScaleText + '%'"></span>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'TheFooter',
+  computed: {
+    pageScaleValue () {
+      return (this.$store.state.pageScale * 100).toFixed(2)
+    },
+    pageScale: {
+      get () {
+        return this.$store.state.pageScale * 100
+      },
+      set (value) {
+        this.$store.commit('setPageScale', value / 100)
+      }
+    }
+  },
+  methods: {
+    zoomUp () {
+      this.$store.commit('setPageScale', this.$store.state.pageScale + 0.1)
+    },
+    zoomDown () {
+      this.$store.commit('setPageScale', this.$store.state.pageScale - 0.1)
+    }
+  }
+}
+</script>
 <style lang="scss" scope>
 @import 'src/assets/scss/_variables';
 
@@ -14,5 +47,35 @@
   right: $right-sidebar-width;
   bottom: 0;
   background: $primary-background-color;
+
+  .footer-resize {
+    float: right;
+    position: relative;
+
+    .footer-slider {
+      width: 250px;
+      padding: 0 100px 0 30px;
+    }
+
+    .icon-button-add, .icon-button-minus, .dp-footer-size-text {
+      position: absolute;
+      top: 8px;
+      color: #20a0ff;
+      cursor: pointer;
+    }
+
+    .icon-button-minus {
+      left: -8px;
+    }
+
+    .icon-button-add {
+      right: 70px;
+    }
+    .dp-footer-size-text {
+      right: 10px;
+      color: #333;
+      font-size: 12px;
+    }
+  }
 }
 </style>
