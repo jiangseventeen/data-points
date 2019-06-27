2733e971d42332b050321287aac70c9a8bf80eb3
jiangseventeen <jiangseventeen@gmail.com>
2019-06-27 15:14:38 +0800

    完成组件图层面板基本功能

--- frontend/src/layout/TheLeftSidebar.vue
+++ frontend/src/layout/TheLeftSidebar.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="left-sidebar">
     <Title>图层</Title>
+
+    <div class="layer-container">
+      <ul class="layer-list">
+        <li
+          :class="{selected: layer.selected}"
+          :key="layer.id"
+          v-for="(layer, index) in layers"
+          @mouseenter="mouseIn(layer)"
+          @mouseleave="mouseOut(layer)"
+          @click="selectComponent(index)"
+        >
+          <div class="layer-thumb"><img :src="layer.thumbnail"/></div>
+          <div class="layer-title" v-text="layer.title"></div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
@@ -8,13 +24,29 @@ import Title from '../components/base/Title'
 
 export default {
   name: 'TheLeftSidebar',
+  computed: {
+    layers () {
+      return this.$store.state.renderComponentList
+    }
+  },
+  methods: {
+    selectComponent (index) {
+      this.$store.commit('selectComponents', index)
+    },
+    mouseIn (layer) {
+      this.$store.commit('hoverComponent', layer)
+    },
+    mouseOut (layer) {
+      this.$store.commit('unhoverComponent', layer)
+    }
+  },
   components: {
     Title
   }
 }
 </script>
 
-<style scope lang="scss">
+<style scoped lang="scss">
 @import 'src/assets/scss/_variables';
 
 .left-sidebar {
@@ -24,5 +56,50 @@ export default {
   width: $left-sidebar-width;
   position: absolute;
   background: $primary-background-color;
+
+
+  .layer-list {
+    margin: 0;
+    padding: 0;
+
+    & > li {
+      height: 50px;
+      line-height: 50px;
+      list-style: none;
+      user-select: none;
+      cursor: pointer;
+
+      .layer-thumb {
+        float: left;
+        width: 40%;
+        height: 100%;
+        padding: 8px 10px;
+        box-sizing: border-box;
+        
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+
+      .layer-title {
+        color: $primary-color;
+        font-size: 12px;
+        font-weight: 400;
+      }
+
+      &.selected {
+        background: $primary-highlight;
+
+        .layer-title {
+          color: $primary-background-color;
+        }
+      }
+
+      &:not(.selected):hover {
+        background: $second-background-color;
+      }
+    }
+  }
 }
 </style>
