6e23035ebf346143e511e6e9de1a826cf847e90f
jiangseventeen <jiangseventeen@gmail.com>
2019-07-08 20:53:19 +0800

    contextmenu 快捷菜单操作组件完成

--- frontend/src/layout/ContextMenu.vue
+++ frontend/src/layout/ContextMenu.vue
@@ -1,29 +1,85 @@
 <template>
   <div class="context-menu">
     <ul class="menu-list">
-      <li><i class="el-icon-upload2"></i>置顶</li>
-      <li><i class="el-icon-download"></i>置底</li>
-      <li><i class="el-icon-top"></i>上移</li>
-      <li><i class="el-icon-bottom"></i>下移</li>
-      <hr>
-      <li><i class="el-icon-folder"></i>新建组</li>
-      <li><i class="el-icon-folder-delete"></i>取消分组</li>
-      <hr>
-      <li><i class="el-icon-lock"></i>锁定</li>
-      <li><i class="el-icon-view"></i>隐藏</li>
-      <hr>
-      <li><i class="el-icon-edit"></i>重命名</li>
-      <li><i class="el-icon-copy-document"></i>复制</li>
-      <li><i class="el-icon-delete"></i>删除</li>
+      <template v-for="(item, i) in menus">
+        <li
+          :key="`${i}-${j}`"
+          v-for="(option, j) in item.options"
+          @click="handleClickMenu(option.action)"
+          :class="{'menu-disabled': option.disabled}"
+        >
+          <i :class="option.icon"></i>{{option.label}}
+        </li>
+        <hr :key="i"/>
+      </template>
     </ul>
   </div>
 </template>
 
 <script>
+import store from '../store'
+
 export default {
   name: 'ContextMenu',
-  data(){
-    return {}
+  store,
+  computed: {
+    selectedComponentList () {
+      return this.$store.getters.selectedComponentList
+    },
+    menus () {
+      let menus = []
+      // 默认图层移动分组可用
+      menus.push({
+        label: '图层移动',
+        options: [
+          { icon: 'el-icon-upload2', label: '置顶', action: 'moveTop', disabled: false },
+          { icon: 'el-icon-download', label: '置底', action: 'moveBottom', disabled: false },
+          { icon: 'el-icon-top', label: '上移', action: 'moveUp', disabled: false },
+          { icon: 'el-icon-bottom', label: '下移', action: 'moveDown', disabled: false },
+        ]
+      })
+
+      /**
+       * @todo 分组功能尚未实现
+       * 此处disabled
+       */
+      menus.push({
+        label: '分组',
+        options: [
+          { icon: 'el-icon-folder', label: '分组', disabled: true },
+          { icon: 'el-icon-folder-delete', label: '取消分组', disabled: true },
+        ]
+      })
+
+      /**
+       * @todo 锁定和隐藏功能尚未实现
+       * 此处disabled
+       */
+      menus.push({
+        label: '锁定',
+        options: [
+          { icon: 'el-icon-lock', label: '锁定', disabled: true },
+          { icon: 'el-icon-view', label: '隐藏', disabled: true },
+        ]
+      })
+
+      // 默认图层操作可用
+      menus.push({
+        label: '图层操作',
+        options: [
+          { icon: 'el-icon-edit', label: '重命名', action: 'renameComponent', disabled: false },
+          { icon: 'el-icon-copy-document', label: '复制', action: 'copyComponents', disabled: false },
+          { icon: 'el-icon-delete', label: '删除', action: 'deleteComponents', disabled: false },
+        ]
+      })
+
+      return menus
+    }
+  },
+  methods: {
+    handleClickMenu (action) {
+      this.$store.commit(action)
+    }
   }
 }
 </script>
@@ -55,10 +111,14 @@ export default {
         margin-right: 8px;
       }
 
-      &:hover {
+      &:not(.menu-disabled):hover {
         border-left: 2px solid $primary-highlight;
         color: $primary-highlight;
         background: $second-background-color;
+      }
+
+      &.menu-disabled {
+        color: transparentize($primary-color, 0.6);
       }
     }
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -53,12 +53,15 @@ const store = new Vuex.Store({
     insertComponent: (state, component) => {
       if (component.enabled) {
         state.renderComponentList.push({
+          enabled: component.enabled,
           componentId: component.id,
           id: _.uniqueId(),
-          hover: false,
-          selected: false,
-          locked: false,
-          isDrop: false,
+          hover: false,                   // 光标hover
+          selected: false,                // 是否选中
+          locked: false,                  // 是否锁定
+          visible: true,                  // 是否隐藏
+          isEdit: false,                  // 是否重命名
+          isDrop: false,                  // 拖拽排序中
           name: component.name,
           title: component.title,
           version: component.version,
@@ -136,8 +139,130 @@ const store = new Vuex.Store({
       let renderComponentList = state.renderComponentList
 
       renderComponentList.splice(to, 0, renderComponentList.splice(from, 1)[0])
-    }
+    },
 
+    /**
+     * 交换两个图层位置
+     * 
+     * @param {Number} index1
+     * @param {Number} index2
+     */
+    layerSwap (state, { index1, index2 }) {
+      if (index1 !== index2) {
+        let component = state.renderComponentList[index1]
+        Vue.set(state.renderComponentList, index1, state.renderComponentList[index2])
+        Vue.set(state.renderComponentList, index2, component)
+      }
+    },
+    /**
+     * 上移选中的图层
+     */
+    moveUp (state) {
+
+      let renderList = state.renderComponentList
+      let selectedList = this.getters.selectedComponentList
+
+      // 上移时正序排列索引数组
+      let activeIndexs = selectedList
+        .map(n => renderList.indexOf(n))
+        .sort((a, b) => a - b)
+
+      activeIndexs.forEach(n => {
+        if (n > 0 && !renderList[n - 1].selected) {
+          this.commit('layerSwap', {index1: n - 1, index2: n})
+        }
+      })
+    },
+
+    /**
+     * 下移选中的图层
+     */
+    moveDown (state) {
+      let renderList = state.renderComponentList
+      let selectedList = this.getters.selectedComponentList
+
+      // 下移时逆序排列索引数组
+      let activeIndexs = selectedList
+        .map(n => renderList.indexOf(n))
+        .sort((a, b) => b - a)
+
+      activeIndexs.forEach(n => {
+        if (
+          n < renderList.length &&
+          renderList[n + 1] &&
+          !renderList[n + 1].selected
+        ) {
+          this.commit('layerSwap', {index1: n, index2: n + 1})
+        }
+      })
+    },
+
+    /**
+     * 置顶选中的图层
+     */
+    moveTop (state) {
+      let renderList = state.renderComponentList
+      let selectedList = this.getters.selectedComponentList
+
+      // 上移时正序排列索引数组
+      let activeIndexs = selectedList
+        .map(n => renderList.indexOf(n))
+        .sort((a, b) => a - b)
+
+      activeIndexs.forEach((n, i) => {
+        this.commit('layerSwap', {index1: n, index2: i})
+      })
+    },
+
+    /**
+     * 置底选中的图层
+     */
+    moveBottom (state) {
+      let renderList = state.renderComponentList
+      let selectedList = this.getters.selectedComponentList
+
+      // 下移时逆序排列索引数组
+      let activeIndexs = selectedList
+        .map(n => renderList.indexOf(n))
+        .sort((a, b) => b - a)
+
+      activeIndexs.forEach((n, i) => {
+        this.commit('layerSwap', {index1: n, index2: renderList.length - 1 - i})
+      })
+    },
+
+    /**
+     * 删除选中组件图层
+     */
+    deleteComponents (state) {
+      let renderList = state.renderComponentList
+      let selectedList = this.getters.selectedComponentList
+
+      selectedList.forEach(n => {
+        let index = renderList.indexOf(n)
+        state.renderComponentList.splice(index, 1)
+      })
+    },
+
+    /**
+     * 复制选中图层
+     */
+    copyComponents (state) {
+      let selectedList = this.getters.selectedComponentList
+
+      selectedList.forEach(n => {
+        this.commit('insertComponent', n)
+      })
+    },
+
+    /**
+     * 重命名选中图层，多个选中图层时，默认修改第一个选中图层
+     */
+    renameComponent (state) {
+      let selectedList = this.getters.selectedComponentList
+      selectedList[0].isEdit = true
+    }
+    
   }
 });
 
