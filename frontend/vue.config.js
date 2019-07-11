4cead8a9e11abf7174aabbb567bd359576f3ad97
jiangseventeen <jiangseventeen@gmail.com>
2019-07-11 23:10:27 +0800

    webpack添加资源路径alias

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,14 @@
+var path = require('path');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? './'
-    : '/'
+    : '/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'src': path.resolve(__dirname, 'src/'),
+      }
+    }
+  }
 }
