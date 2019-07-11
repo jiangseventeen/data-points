36b3806cd63ff06100cd51f2c480f60bcaed57fe
jiangseventeen <jiangseventeen@gmail.com>
2019-07-11 23:15:11 +0800

    统一常用常量字典值

---
+++ frontend/src/constants/fontFamily.js
@@ -0,0 +1,8 @@
+export default [
+  { value: 'Microsoft yahei', label: '微软雅黑' },
+  { value: 'songti', label: '宋体' },
+  { value: 'heiti', label: '黑体' },
+  { value: 'lishu', label: '隶书' },
+  { value: 'youyuan', label: '幼圆' },
+  { value: 'Arial', label: 'Arial'}
+]
---
+++ frontend/src/constants/fontWeight.js
@@ -0,0 +1,5 @@
+export default [
+  { value: 700, label: '加粗' },
+  { value: 500, label: '正常' },
+  { value: 100, label: '纤细' }
+]
