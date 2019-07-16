32113a390a777b31732f762b08df2a23b351f08b
jiangseventeen <jiangseventeen@gmail.com>
2019-07-16 22:49:26 +0800

    添加百分比滑动输入

---
+++ frontend/src/components/base/PercentSlider.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-slider
+    :value="parseInt(value)"
+    @input="handleInput"
+    :format-tooltip="formatTooltip"
+  />
+</template>
+
+<script>
+export default {
+  name: 'PercentSlider',
+  props: ['value'],
+  methods: {
+    handleInput (value) {
+      this.$emit('input', `${value}%`)
+    },
+    formatTooltip (value) {
+      return `${value}%`
+    }
+  }
+}
+</script>
