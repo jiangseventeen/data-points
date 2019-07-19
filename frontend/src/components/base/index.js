d6529a00aba7c546b455af2eb7ff8c30415d3c00
jiangseventeen <jiangseventeen@gmail.com>
2019-07-19 21:58:01 +0800

    增加全局注册组件

--- frontend/src/components/base/index.js
+++ frontend/src/components/base/index.js
@@ -3,8 +3,22 @@
  */
 
 import Vue from 'vue'
+import Icon from './Icon'
 import DpColor from './DpColor'
+import AxisName from './AxisName'
+import ChartGrid from './ChartGrid'
+import AxisLabel from './AxisLabel'
+import PercentSlider from './PercentSlider'
+import ChartSplitLine from './ChartSplitLine'
 import ComponentTitle from './ComponentTitle'
+import CoordinateConfig from './CoordinateConfig'
 
+Vue.component('Icon', Icon)
 Vue.component('DpColor', DpColor)
+Vue.component('AxisName', AxisName)
+Vue.component('ChartGrid', ChartGrid)
+Vue.component('AxisLabel', AxisLabel)
+Vue.component('PercentSlider', PercentSlider)
+Vue.component('ChartSplitLine', ChartSplitLine)
 Vue.component('ComponentTitle', ComponentTitle)
+Vue.component('CoordinateConfig', CoordinateConfig)
