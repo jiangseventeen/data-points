7e9bce6cc2f53152bdcd7d28e6be73ab0209134c
jiangseventeen <jiangseventeen@gmail.com>
2019-09-23 11:33:29 +0800

    feat(frontend): add constant fontStyle fontWeight

--- frontend/src/components/base/DpFormItem.vue
+++ frontend/src/components/base/DpFormItem.vue
@@ -1,9 +1,9 @@
 <template>
   <el-row class="dp-form-item" type="flex" align="middle">
-    <el-col :span="8">
+    <el-col :span="7">
       <slot name="label"></slot>
     </el-col>
-    <el-col :span="16">
+    <el-col :span="17">
       <slot name="control"></slot>
     </el-col>
   </el-row>
--- frontend/src/constants/fontWeight.js
+++ frontend/src/constants/fontWeight.js
@@ -1,5 +1,12 @@
 export default [
-  { value: 700, label: '加粗' },
-  { value: 500, label: '正常' },
-  { value: 100, label: '纤细' }
+  { value: 'bold', label: '加粗' },
+  { value: 'normal', label: '正常' },
+  { value: 'lighter', label: '纤细' },
+  { value: 100, label: '100' },
+  { value: 200, label: '200' },
+  { value: 300, label: '300' },
+  { value: 400, label: '400' },
+  { value: 500, label: '500' },
+  { value: 600, label: '600' },
+  { value: 700, label: '700' }
 ]
