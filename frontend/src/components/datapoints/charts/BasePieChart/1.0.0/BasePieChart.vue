bcad5ac98392b8a1e8803396718162a21e7c8fb3
jiangseventeen <jiangseventeen@gmail.com>
2019-10-09 17:15:45 +0800

    style(frontend): format comments

--- frontend/src/components/datapoints/charts/BasePieChart/1.0.0/BasePieChart.vue
+++ frontend/src/components/datapoints/charts/BasePieChart/1.0.0/BasePieChart.vue
@@ -10,18 +10,18 @@ import _ from 'lodash'
 import commonMixin from 'src/components/datapoints/mixins/common'
 
 const mapPositionToOption = {
-  "1": { orient: 'horizontal', left: 'left', top: '2%' },        //顶部左对齐
-  "2": { orient: 'horizontal', left: 'center', top: '2%' },      //顶部居中
-  "3": { orient: 'horizontal', left: 'right', top: '2%' },       //顶部右对齐
+  "1": { orient: 'horizontal', left: 'left', top: '2%' },         //顶部左对齐
+  "2": { orient: 'horizontal', left: 'center', top: '2%' },       //顶部居中
+  "3": { orient: 'horizontal', left: 'right', top: '2%' },        //顶部右对齐
   "4": { orient: 'vertical', top: 'top', left: '2%' },            //左侧上对齐
   "5": { orient: 'vertical', top: 'middle', left: '2%' },         //左侧居中
   "6": { orient: 'vertical', top: 'bottom', left: '2%' },         //左侧下对齐
-  "7": { orient: 'vertical', top: 'top', right: '2%' },            //右侧上对齐
-  "8": { orient: 'vertical', top: 'middle', right: '2%' },         //右侧居中
-  "9": { orient: 'vertical', top: 'bottom', right: '2%' },         //右侧下对齐
-  "10": { orient: 'horizontal', left: 'left', bottom: '2%' },       //底部左对齐
-  "11": { orient: 'horizontal', left: 'center', bottom: '2%' },     //底部居中
-  "12": { orient: 'horizontal', left: 'right', bottom: '2%' },      //底部右对齐
+  "7": { orient: 'vertical', top: 'top', right: '2%' },           //右侧上对齐
+  "8": { orient: 'vertical', top: 'middle', right: '2%' },        //右侧居中
+  "9": { orient: 'vertical', top: 'bottom', right: '2%' },        //右侧下对齐
+  "10": { orient: 'horizontal', left: 'left', bottom: '2%' },     //底部左对齐
+  "11": { orient: 'horizontal', left: 'center', bottom: '2%' },   //底部居中
+  "12": { orient: 'horizontal', left: 'right', bottom: '2%' },    //底部右对齐
 }
 
 export default {
